6fd639cbc1e741ca21071aa2b9223cd03b0c5aae
alexiseichst <alexiseichst@gmail.com>
2022-09-26 11:49:42 +0200

    Pypi publish informations added

--- setup.py
+++ setup.py
@@ -3,11 +3,8 @@ from setuptools import setup
 from pathlib import Path
 import os
 
-def __get_current_dir():
-    return os.path.dirname(os.path.realpath(__file__))
-
-def __get_install_requires(file):
-    requirements = Path(os.path.join(__get_current_dir(), file)).read_text()
+def __get_install_requires(rtype):
+    requirements = Path(f"{rtype}-requirements.txt").read_text()
     return requirements.split()
 
 setup(
@@ -16,13 +13,13 @@ setup(
     author='xtrembuffalo',
     author_email='tristan.cladet@gmail.com',
     description='Handle generic file and directories caching',
-    long_description=Path(os.path.join(__get_current_dir(), 'README.md')).read_text(),
+    long_description=Path('README.md').read_text(),
     long_description_content_type='text/markdown',
     url='https://github.com/xtrembuffalo/xtremcache',
-    install_requires=__get_install_requires('dist-requirements.txt'),
+    install_requires=__get_install_requires('dist'),
     entry_points={
         'console_scripts': ['xtremcache=xtremcache.__main__:main']
     },
-    tests_require=__get_install_requires('test-requirements.txt'),
+    tests_require=__get_install_requires('test'),
     test_suite='tests'
 )
