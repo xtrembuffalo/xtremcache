621bcf0f8512723af76ab534c9e31760ad226f09
Alexis EICHSTADT <alexis.eichstadt@siemens.com>
2022-12-20 10:57:13 +0100

    Revert "setup.py in venv"
    This reverts commit ee2638dbad197d725e9932a007549a839f5a20c2.

--- setup.py
+++ setup.py
@@ -1,29 +1,24 @@
 import os
 from pathlib import Path
+
 from setuptools import setup
-from functools import lru_cache
 
-@lru_cache
-def get_version_info(item):
-    version={}
-    exec(Path(os.path.join('xtremcache', 'version.py')).read_text(), version)
-    return version[item]
 
-@lru_cache
-def get_long_description():
-    return Path('README.md').read_text()
+def __get_install_requires(rtype):
+    requirements = Path(f'{rtype}-requirements.txt').read_text()
+    return requirements.split()
 
-@lru_cache
-def get_install_requires():
-    return Path(f'dist-requirements.txt').read_text().split()
-    
+version = {}
+with open(os.path.join('xtremcache', 'version.py')) as fp:
+    exec(fp.read(), version)
+
 setup(
-    name=get_version_info('__app_name__'),
-    version=get_version_info('__version__'),
-    author=get_version_info('__author__'),
+    name=version['__app_name__'],
+    version=version['__version__'],
+    author=version['__author__'],
     author_email='tristan.cladet@gmail.com',
     description='Handle generic file and directories caching',
-    long_description=get_long_description(),
+    long_description=Path('README.md').read_text(),
     long_description_content_type='text/markdown',
     license='GPL-3.0',
     classifiers=[
@@ -48,7 +43,7 @@ setup(
         'Source': 'https://github.com/xtrembuffalo/xtremcache',
         'Tracker': 'https://github.com/xtrembuffalo/xtremcache/issues',
     },
-    install_requires=get_install_requires(),
+    install_requires=__get_install_requires('dist'),
     entry_points={
         'console_scripts': ['xtremcache=xtremcache.__main__:main']
     },
@@ -68,15 +63,8 @@ setup(
                 os.path.join('xtremcache','ext','msys2','unzip','v6.00','bin','msys-2.0.dll'),
                 os.path.join('xtremcache','ext','msys2','unzip','v6.00','bin','msys-bz2-1.dll'),
             ]
-        ),
-        (
-            os.path.join('..','..'),
-            [
-                'dist-requirements.txt',
-                'README.md'
-            ]
         )
     ],
     python_requires='>=3.8',
-    tests_require=get_install_requires(),
+    tests_require=__get_install_requires('test'),
     test_suite='tests')
